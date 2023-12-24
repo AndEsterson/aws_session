3991bfe3ea300bc605fadd3610b4f30b33d9fb16
Andrew Esterson <andrewesterson1@gmail.com>
2023-12-24 12:38:37 +0000

    working aws_session.sh script

---
+++ aws_session.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Global vars
+role_arn="<role-arn>"
+profile="default"
+serial_number="<serial_number>"
+
+if [[ $1 = "exit" ]]; then
+    unset AWS_ACCESS_KEY_ID
+    unset AWS_SECRET_ACCESS_KEY
+    unset AWS_SESSION_TOKEN
+    export PS1=$PS1_PRE_AWS_SESSION
+else
+    # Run the AWS CLI command and store the JSON response
+    echo -n "mfa token: "
+    read token
+    aws_output=$(aws sts assume-role --role-arn $role_arn --role-session-name "session-role" --profile $profile --serial-number $serial_number --token-code $token)
+
+    # Parse the JSON response and extract values
+    access_key=$(echo $aws_output | jq -r .Credentials.AccessKeyId)
+    secret_key=$(echo $aws_output | jq -r .Credentials.SecretAccessKey)
+    session_token=$(echo $aws_output | jq -r .Credentials.SessionToken)
+
+    # Export values as environment variables
+    export AWS_ACCESS_KEY_ID=$access_key
+    export AWS_SECRET_ACCESS_KEY=$secret_key
+    export AWS_SESSION_TOKEN=$session_token
+    export PS1_PRE_AWS_SESSION=$PS1
+    export PS1="%{$fg[yellow]%}%n%{$reset_color%}@%{$fg[yellow]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+fi
---
+++ readme.md
@@ -0,0 +1,9 @@
+# AWS session
+this is a very simple (but handy) bash script that prompts for an mfa code, asks for creds for a role that requires mfa, then setsthese. 
+
+Helpful for Terraform/Opentofu, where handling mfa/assumed roles can be a pain.
+
+You can `source ./aws_session.sh` to enter and then `source ./aws_session.sh exit` to exit, the idea is that you'll probably alias `source /some/path/aws_session.sh` to make your life easier (I use `alias awss=source /some/path/aws_session.sh`). 
+
+#TO DO
+make this read from aws credentials and take multiple possible accounts, instead of needing creds manaully added
