4aa377e345fdc6e80fae7308116ab10e7e6c2381
Andrew Esterson <andrewesterson1@gmail.com>
2023-12-29 11:45:25 +0000

    allowed configurable PS1 on a per role basis

--- aws_session.sh
+++ aws_session.sh
@@ -10,7 +10,9 @@ if [[ $1 = "exit" ]]; then
     unset AWS_ACCESS_KEY_ID
     unset AWS_SECRET_ACCESS_KEY
     unset AWS_SESSION_TOKEN
-    export PS1=$PS1_PRE_AWS_SESSION
+    if [[ -n "PS1_PRE_AWS_SESSION" ]]; then
+        export PS1=$PS1_PRE_AWS_SESSION
+    fi
 else
     if [[ -z "$1" ]]; then
         profile="default"
@@ -20,6 +22,7 @@ else
     role_arn="$(aws configure get $profile.role_arn)"
     source_profile="$(aws configure get $profile.source_profile)"
     serial_number="$(aws configure get $profile.mfa_serial)"
+    session_color="$(aws configure get $profile.session_color)"
     if [[ -z $role_arn || -z $source_profile || -z $serial_number ]]; then
         >&2 echo 'the profile for the role being assumed must have a role_arn, source_profile, serial number'
     else
@@ -27,7 +30,6 @@ else
         read token
         aws_output=$(aws sts assume-role --role-arn $role_arn --role-session-name "session-role" --profile $source_profile --serial-number $serial_number --token-code $token)
 
-        # Parse the JSON response and extract values
         access_key=$(echo $aws_output | jq -r .Credentials.AccessKeyId)
         secret_key=$(echo $aws_output | jq -r .Credentials.SecretAccessKey)
         session_token=$(echo $aws_output | jq -r .Credentials.SessionToken)
@@ -35,11 +37,12 @@ else
     if [[ -z $access_key || -z $secret_key || -z $session_token ]]; then
         >&2 echo 'assume-role failed'
     else
-            # Export values as environment variables
             export AWS_ACCESS_KEY_ID=$access_key
             export AWS_SECRET_ACCESS_KEY=$secret_key
             export AWS_SESSION_TOKEN=$session_token
-            export PS1_PRE_AWS_SESSION=$PS1
-            export PS1="%{$fg[yellow]%}%n%{$reset_color%}@%{$fg[yellow]%}%m %{$fg[yellow]%}%~ %{$reset_color%}%% "
+            if [[ -n "$session_color" ]]; then
+                export PS1_PRE_AWS_SESSION=$PS1
+                export PS1="%{$fg[$session_color]%}%n%{$reset_color%}@%{$fg[$session_color]%}%m %{$fg[$session_color]%}%~ %{$reset_color%}%% "
+            fi
     fi
 fi
--- readme.md
+++ readme.md
@@ -5,4 +5,6 @@ Helpful for Terraform/Opentofu, where handling mfa/assumed roles can be a pain.
 
 You can `source ./aws_session.sh <profile>` to enter and then `source ./aws_session.sh exit` to exit, the idea is that you'll probably alias `source /some/path/aws_session.sh` to make your life easier (I use `alias awss=source /some/path/aws_session.sh`). 
 
-The profile argument corresponds to a profile in your aws credentials file, it must specify the role_arn, source_profile and mfa_serial (but probably doesn't need anything else). If no argument is supplied, it will attempt to assume the default profile (i.e `call aws configure get` without a profile specified)
+The profile argument corresponds to a profile in your aws credentials file, it must specify the role_arn, source_profile and mfa_serial (but probably doesn't need anything else). If no argument is supplied, it will attempt to assume the default profile (i.e `call aws configure get` without a profile specified).
+
+You can optionally specify a `session_color` in your aws credentials file, e.g `session_color = yellow` this will be used to update `$PS1` until you exit the session, helping to track which profile is in use. 
